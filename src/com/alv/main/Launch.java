8c5bf30abe4017adb857debafe2f9d4c10acd263
Alvaro Andres Obregon Carreño <alvaro.obregon>
2022-05-18 14:07:21 -0500

    Test maven repository GCloud

---
+++ src/com/alv/main/Launch.java
@@ -0,0 +1,11 @@
+package com.alv.main;
+
+import com.alv.util.UtilFiles;
+
+public class Launch {
+
+	public static void main(String args[]) {
+		System.out.println("Inciiando desde el maven " + UtilFiles.getCurrentPath());
+	}
+	
+}
